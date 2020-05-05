083eaa36a4ab52cfb973a942c2f54a1d5a9672d6
Anshuman Anand <anshu007r@gmail.com>
2020-05-05 16:35:23 +0530

    WaterFall

--- WaterFall.js
+++ WaterFall.js
@@ -1,11 +1,29 @@
 const process = require('process');
 const file = require('fs');
 const http = require('http');
+const async = require('async');
+async.waterfall([
+    function(execute){
+        file.readFile(process.argv[2],function(error,data){
+          if(error) return done(error);
+          return execute(null,data);
+        })
+    },
+    function(data,execute){
+        let body=''
+        http.get(data.toString(),function(response){
+            response.on('data',function(chunk){
+                body+=chunk.toString()
+            });
+            response.on('end',function(chunk){
+                execute(null,body)
+            });
+        }).on('error',function(error){
+            done(error);
+        });
+    }
 
-const fileName = process.argv[2];
-const fileContent=file.readFileSync(fileName);
-if(fileContent){
-    http.get(fileContent,function(res){
-        console.log(res);
-    })
-}
+],function(error,data){
+    if (error) return console.error(error);
+    console.log(data) 
+})
